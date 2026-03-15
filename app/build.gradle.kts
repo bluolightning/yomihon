88d3766794c89d144fab281c20427d651be5e559
bluolightning <bluolightning@proton.me>
2026-03-15 19:08:35 -0400

    Release v0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
     defaultConfig {
         applicationId = "app.yomihon"
 
-        versionCode = 16
-        versionName = "0.2.0"
+        versionCode = 17
+        versionName = "0.2.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
